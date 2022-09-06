e735ddf5bc15a877d5f4c62b98475a1b9a0a3a03
guming <59330624+guming-Github@users.noreply.github.com>
2022-09-06 20:48:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,83 +1,46 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
 
-  // 公众号APP_ID 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
    APP_ID: "wx95a939c0c88c5907",
-
-  // 公众号APP_SECRET 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-    APP_SECRET: "8f36780751b3a59f7611f131ae81b9f8",
+   APP_SECRET: "8f36780751b3a59f7611f131ae81b9f8",
 
   USERS: [
     {
-      // 想要发送的人的名字
       name: "老婆0",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "odS7L542WxnLSJZRwcGCEYFb014Q",
-      // 你想对他发送的模板消息的模板ID
       useTemplateId: "Lq-pwjTgoQQDRUOZUPC48mt9Zdk2qI8LtKQoBDxFhcw",
-      // 所在省份
       province: "河南",
-      // 所在城市
       city: "焦作",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.yuque.com/dashboard"
+      //openUrl: "https://www.yuque.com/dashboard"
     }
   ],
 
-/**
-   回调消息 相关，主要用来展示发送是否成功/失败的数据
+
  
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: "Lq-pwjTgoQQDRUOZUPC48mt9Zdk2qI8LtKQoBDxFhcw",
-
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
-      // 一般都填自己
+
       name: "自己",
-      // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "odS7L542WxnLSJZRwcGCEYFb014Q",
     }, 
   ],
-*/
+
     
   FESTIVALS: [
     {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
   ],
 
-  /**
-   * 限制重要节日的展示条目, 需要填写数字; 
-   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-   * 如果为0, 则默认展示全部
-   */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 0,
 
-  /** 日期相关 */
 
-  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
-    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
-    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
     {"keyword": "love_day", date: "2015-05-01"}
   ],
 
-  /** 插槽 */
-
-  /** 你可以在这里写超多的你想显示的内容了！
-    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }} 
-    * */
   SLOT_LIST: [
-    // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
       "因为太喜欢你，所以看谁都像是情敌。",
       "申请成为你爱里的永久居民。",
@@ -86,8 +49,6 @@ export const config = {
      "你主要的问题在于读书太少而想得太多",
     ]},
   ],
-
-  /** 每日一言 */
 
   // 好文节选的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
