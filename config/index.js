b17d24a07d05995b163bec95459bdf2c9fdfb5dc
guming <59330624+guming-Github@users.noreply.github.com>
2022-09-08 18:45:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ export const config = {
   USERS: [
     {
       name: "老婆",
-      //id: "odS7L59v22TX_GTOHi-Rd0-2YXWM",
-      id: "odS7L542WxnLSJZRwcGCEYFb014Q",
+      id: "odS7L59v22TX_GTOHi-Rd0-2YXWM",
+      //id: "odS7L542WxnLSJZRwcGCEYFb014Q",
       useTemplateId: "Pn3pivmhxRhMSYYeI76_nen-XwXsZm0Ki7pHyUYda9Y",
       province: "江西",
       city: "上饶",
